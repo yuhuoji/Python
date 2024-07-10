7a33130b29234763b434da16178fd53661b05861
yuhuoji <yuhuoji@outlook.com>
2024-07-10 18:03:35 +0800

    update

--- src/0705/markXls2.py
+++ src/0705/markXls2.py
@@ -1,38 +1,48 @@
+import datetime
 import pandas as pd
-import numpy as np
 import os
-
 
 def process_excel(file_path):
     # 读取 Excel 文件
     df = pd.read_excel(file_path)
 
-    # 定义关键词列表
-    keywords = ['传媒', '地产', '房产', '家庭', '教育', '金融', '军事', '科技', '科普', '科学',
-                '历史', '媒体', '评测种草', '人文', '人物', '企业', '商业', '时事', '时政',
-                '数码', '新闻', '原创', '战术', '杂志', '职场', '财经', '政治']
+    # 定义关键词列表和类型
+    keywords = [
+         '旅行'
+    ]
+    type = '生活购物'
 
-    # 创建一个函数来检查是否包含关键词
+    # 创建一个函数来检查文本是否包含任意关键词
     def contains_keyword(text):
         if isinstance(text, str):
             return any(keyword in text for keyword in keywords)
         return False
 
-    # 对于 merged_category 为空且 cyber_star_name 包含关键词的行，填入 "社会时政"
-    mask = (df['merged_category'].isna() | df['merged_category'].eq('')) & df['cyber_star_name'].apply(contains_keyword)
-    df.loc[mask, 'merged_category'] = '社会时政'
+    # 创建掩码：对于 merged_category 为空且 cyber_star_name 或 description 中包含关键词的行，填入 '艺术文化'
+    mask = (df['merged_category'].isna() | df['merged_category'].eq('')) & (
+        df['cyber_star_name'].apply(contains_keyword) |
+        df['description'].apply(contains_keyword) |
+        df['user_name'].apply(contains_keyword)
+    )
+    df.loc[mask, 'merged_category'] = type
 
-    # 创建新文件名
-    file_name = os.path.basename(file_path)
-    new_file_name = f"updated_1.2_{file_name}"
+    # 统计处理完后的 merged_category 列为空的数量
+    empty_count = df['merged_category'].isna().sum() + df['merged_category'].eq('').sum()
+
+    # 获取当前日期和时间，格式化时间戳
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("%Y%m%d_%H%M")
+
+    # 生成新文件名并保存在原文件夹中
+    new_file_name = f"updated_{formatted_time}_网红账号打标0705.xlsx"
     new_file_path = os.path.join(os.path.dirname(file_path), new_file_name)
 
     # 保存到新文件
     df.to_excel(new_file_path, index=False)
 
     print(f"处理完成。新文件保存为: {new_file_path}")
-
+    print(f"merged_category 列为空的数量: {empty_count}")
 
 # 使用示例
-file_path = "C:/Users/yuhuo/Documents/Code/pythonProject0619/src/0705/updated_1.1_网红账号打标0705.xlsx"
+file_path = "C:/Users/yuhuo/Documents/Code/pythonProject0619/src/0705/updated_1.3_网红账号打标0705.xlsx"
 process_excel(file_path)
