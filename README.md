73142032fc0ec74b30e02019425d7f20c26cbec5
yuhuoji <yuhuoji@outlook.com>
2025-11-24 16:10:57 +0800

    update leetcode plugin config

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Python
+
+TempFilePath:
+` /Users/yuhuoji/Code/Python/src/leetcode`
+
+``
+LC$!velocityTool.deleteWhitespace("${question.frontendQuestionId}${velocityTool.camelCaseName(${question.titleSlug})}") 
+``
+
+``
+
+"""
+${question.frontendQuestionId} ${question.title}
+"""
+
+${question.code}
+
+``
---
+++ src/leetcode/leetcode/editor/cn/LC1TwoSum .py
@@ -0,0 +1,20 @@
+"""
+1 两数之和
+"""
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        return []
+# leetcode submit region end(Prohibit modification and deletion)
