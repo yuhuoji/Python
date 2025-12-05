13f03660d98bbba38cb2089c35fd2bf8025ac1a9
yuhuoji <yuhuoji@outlook.com>
2025-12-05 19:15:02 +0800

    update

--- src/leetcode/editor/cn/LC2AddTwoNumbers.py
+++ src/leetcode/editor/cn/LC2AddTwoNumbers.py
@@ -2,7 +2,10 @@
 2 两数相加
 """
 from typing import *
+from src.leetcode.lc_utils import *
 
+
+# TODO @date 2025-12-05
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
 # class ListNode:
@@ -10,7 +13,33 @@ from typing import *
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry=0) -> Optional[ListNode]:
+        if l1 is None and l2 is None:
+            return ListNode(carry) if carry else None
+        if l1 is None:
+            l1, l2 = l2, l1
+        s = carry + l1.val + (l2.val if l2 else 0)
+        l1.val = s % 10
+        l1.next = self.addTwoNumbers(l1.next, l2.next if l2 else None, s // 10)
+        return l1
+
+    def addTwoNumbers1(self, l1: Optional[ListNode], l2: Optional[ListNode], carry=0) -> Optional[ListNode]:
+        if l1 is None and l2 is None and carry == 0:
+            return None
+        s = carry
+        if l1:
+            s += l1.val
+            l1 = l1.next
+        if l2:
+            s += l2.val
+            l2 = l2.next
+        return ListNode(s % 10, self.addTwoNumbers(l1, l2, s // 10))
+
 
 # leetcode submit region end(Prohibit modification and deletion)
 
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    pass
--- src/leetcode/editor/cn/LC53MaximumSubarray.py
+++ src/leetcode/editor/cn/LC53MaximumSubarray.py
@@ -1,13 +1,36 @@
 """
 53 最大子数组和
 """
+import ast
+from cmath import inf
+from typing import *
+from src.leetcode.lc_utils import *
+
 
 # leetcode submit region begin(Prohibit modification and deletion)
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+class Solution:
+    def maxSubArray1(self, nums: List[int]) -> int:
+        f = [0] * len(nums)
+        f[0] = nums[0]
+        for i in range(1, len(nums)):
+            f[i] = max(f[i - 1], 0) + nums[i]
+        return max(f)
+
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans = -inf
+        f = 0
+        for x in nums:
+            f = max(f, 0) + x
+            ans = max(ans, f)
+        return ans
+
+
 # leetcode submit region end(Prohibit modification and deletion)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    # [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    intput = "[-2,1,-3,4,-1,2,1,-5,4]"
+    print(solution.maxSubArray(ast.literal_eval(intput)))
+    pass
