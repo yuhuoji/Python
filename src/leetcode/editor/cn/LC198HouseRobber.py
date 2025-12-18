37163913e555e1131e865be68ab993fbc8375266
yuhuoji <yuhuoji@outlook.com>
2025-12-18 15:37:13 +0800

    update

---
+++ src/leetcode/editor/cn/LC198HouseRobber.py
@@ -0,0 +1,47 @@
+"""
+198 打家劫舍
+"""
+from linecache import cache
+from typing import *
+from src.leetcode.lc_utils import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def rob1(self, nums: List[int]) -> int:
+        n = len(nums)
+        f = [0] * (n + 2)
+        for i, x in enumerate(nums):
+            f[i] = max(x + f[i - 2], f[i - 1])
+        return f[n - 1]
+
+    def rob2(self, nums: List[int]) -> int:
+        n = len(nums)
+        f = [0] * (n + 2)
+        for i, x in enumerate(nums):
+            f[i + 2] = max(x + f[i], f[i + 1])
+        return f[n + 1]
+
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        cache = [-1] * n
+
+        def dfs(i):
+            if i < 0:
+                return 0
+            if cache[i] != -1:
+                return cache[i]
+            res = max(dfs(i - 1), nums[i] + dfs(i - 2))
+            cache[i] = res
+            return res
+
+        return dfs(n - 1)
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    pass
