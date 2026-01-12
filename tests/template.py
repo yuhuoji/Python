f68c00ad3a4963b1215e029ecdbf42c205c3d6d5
yuhuoji <yuhuoji@outlook.com>
2026-01-12 21:08:03 +0800

    update template

---
+++ tests/template.py
@@ -0,0 +1,16 @@
+import array
+import bisect
+import collections
+from linecache import cache
+from math import inf
+from typing import *
+from src.leetcode.lc_utils import *
+
+
+class Solution:
+    pass
+
+
+if __name__ == "__main__":
+    soluton = Solution()
+    pass
